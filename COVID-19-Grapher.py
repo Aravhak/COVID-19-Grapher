d40c367c655e1365016e2fa255b9989b42720939
Arav <haka@asbindia.org>
2020-04-06 22:43:15 +0530

    Final Coding Project
    I fixed the graph finally. The project is cleaned up. I commented the code.

--- COVID-19-Grapher.py
+++ COVID-19-Grapher.py
@@ -3,8 +3,10 @@ from bs4 import BeautifulSoup
 import matplotlib.pyplot as plt; plt.rcdefaults()
 import numpy as np
 import matplotlib.pyplot as plt
+import operator
 
 countries = {}
+
 
 URL = 'https://www.worldometers.info/coronavirus/'
 
@@ -14,31 +16,26 @@ page = requests.get(URL, headers=headers)
  
 soup = BeautifulSoup(page.content, 'html.parser')
 
-#country1Count = soup.find_all(style="font-weight: bold; text-align:right").get_text()
 
-#print (country1Count)
-
+#Web Scraper
 Table = soup.findChildren("table")
 
 my_table = Table[0]
 
 rows = my_table.findChildren(['tr'])
 
-#print(rows)
-
 for row in rows[2:7]:
     cells = row.findChildren(['td'])
-    countries[cells[0].string] = cells[1].string
+    countries[cells[0].string] = float((cells[1].string).replace(',',''))
+    
 
-#print(countries)
-    
-    
+   
+#Bar Graph Set Up 
 labels = countries.keys()
 cases = countries.values()
 
-#print(labels)
-
 y_pos = np.arange(len(labels))
+
 
 plt.bar(y_pos, cases, align='center', alpha=0.5)
 plt.xticks(y_pos, labels)
@@ -47,16 +44,3 @@ plt.title('COVID-19 Cases by Countries')
 
 plt.show()
 
-
-#total = soup.find('class="sorting_1"').get_text()
-
-#print(total)
-
-#country2Count = soup.find_all_next(style="font-weight: bold; text-align:right").get_text()
-
-#lineHTML = soup.a
-#countryTestCount = lineHTML.find_all_next(string=True)
-
-#print (countryTestCount)
-
-#print (country2Count)
